fcfcb4f7eeb7f6e9d1d115dbd8f00ee4a1abacfd
MickiMouse <495lolkek42@gmail.com>
2019-02-01 21:49:37 +0200

    Last realization

--- environment.py
+++ environment.py
@@ -9,34 +9,23 @@ class Env:
         print("Environment", "\n", self.env)
 
     def move(self, x: int, y: int):
-        if self.count[x, y] == 0:
-            self.count[x, y] = 1
-            self.count_enemy[x, y] = 1
-        self.env[x, y] = 10
+        if self.count[y, x] == 0 and self.count_enemy[y, x] == 0:
+            self.count[y, x] = 1
+            self.env[y, x] = 10
+        else:
+            raise IndexError("Here is busy")
         print("My move", "\n", self.env)
 
-        if self.check_win():
+        _y, _x = np.unravel_index(self.env.argmin(), self.count.shape)
+        self.enemy_move(_x, _y)
+
+        if self.check_win() or self.check_lose():
             return self.dell()
 
-        x_enemy = np.random.randint(0, 2)
-        y_enemy = np.random.randint(0, 2)
-        self.enemy_move(x_enemy, y_enemy)
-
     def enemy_move(self, x: int, y: int):
-        if self.count_enemy[x, y] == 1 or self.count[x, y] == 1:
-            while True:
-                if self.count_enemy[x, y] == 1 or self.count[x, y] == 1:
-                    x = np.random.randint(0, 2)
-                    y = np.random.randint(0, 2)
-                else:
-                    self.count_enemy[x, y] = 1
-                    self.env[x, y] = 20
-                    print("Enemy move", "\n", self.env)
-                    break
-        else:
-            self.count_enemy[x, y] = 1
-            self.env[x, y] = 20
-            print("Enemy move", "\n", self.env)
+        self.count_enemy[y, x] = 1
+        self.env[y, x] = 20
+        print("Enemy move", "\n", self.env)
 
     def check_win(self):
         for i in range(self.count.shape[0]):
@@ -58,7 +47,28 @@ class Env:
             print("YOU WIN")
             return True
 
+    def check_lose(self):
+        for i in range(self.count_enemy.shape[0]):
+            if np.sum(self.count_enemy[i, :]) == 3:
+                print("YOU LOSE")
+                return True
+            elif np.sum(self.count_enemy[:, i]) == 3:
+                print("YOU LOSE")
+                return True
+
+        if np.sum([self.count_enemy[0, 0],
+                   self.count_enemy[1, 1],
+                   self.count_enemy[2, 2]]) == 3:
+            print("YOU LOSE")
+            return True
+        elif np.sum([self.count_enemy[0, 2],
+                     self.count_enemy[1, 1],
+                     self.count_enemy[2, 0]]) == 3:
+            print("YOU LOSE")
+            return True
+
     def dell(self):
         self.env = np.zeros((3, 3), dtype=int)
         self.count = np.zeros((3, 3), dtype=int)
         self.count_enemy = np.zeros((3, 3), dtype=int)
+        print("Environment", "\n", self.env)
